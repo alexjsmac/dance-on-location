11ca57c4dcb285bb0a405483186f16e75e7d8f70
Alex MacLean <maclean199@gmail.com>
2024-07-17 08:03:04 -0400

    Upgrade angular and rust

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { UploadComponent } from './upload/upload.component';
-import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, UploadComponent, HttpClientModule],
+  imports: [RouterOutlet, LoginComponent, UploadComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
